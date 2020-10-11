753aeeaadbde401ddde378248d7febe97c73ba5a
Lasse Lautanala <lasselautanala@Lasse-Mac-mini.local>
2020-10-11 12:02:49 +0300

    V02

--- cal.py
+++ cal.py
@@ -1,12 +1,12 @@
 from db import db
-from datetime import date, timedelta 
+from datetime import date, timedelta
 
 
 def get_date(date_id):
     sql = "SELECT date FROM calendar WHERE id = :date_id"
     result = db.session.execute(sql, {"date_id":date_id})
     return result.fetchone()[0]
-
+    
 def get_today():
     today = date.today()
     sql = "SELECT id FROM calendar WHERE date = :today"
@@ -16,15 +16,14 @@ def get_today():
 def get_week():
     today = date.today()
     day7 = date.today() + timedelta(days=7)
-    sql = "SELECT week_nr FROM calendar WHERE date > :today AND date < :day7 AND work_day = TRUE ORDER BY date"
+    sql = "SELECT week_nr FROM calendar WHERE date > :today AND date < :day7 AND work_day = TRUE"
     result = db.session.execute(sql, {"today":today, "day7":day7})
     return result.fetchone()[0]
 
 def get_next_week(week_nr, ndays):
-    sql = "SELECT date FROM calendar WHERE week_nr = :week_nr ORDER BY date"
+    sql = "SELECT date FROM calendar WHERE week_nr = :week_nr"
     result = db.session.execute(sql, {"week_nr":week_nr})
-    day7 = result.fetchone()[0]
-    day7 = day7 + timedelta(days=ndays)
+    day7 = result.fetchone()[0] + timedelta(days=ndays)
     sql = "SELECT week_nr FROM calendar WHERE date = :day7"
     result = db.session.execute(sql, {"day7":day7})
     week_nr = result.fetchone()[0]
