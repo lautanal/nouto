070e181bec8a931206c8c7af8d79d7361d5ad421
Lasse Lautanala <lasselautanala@Lasse-Mac-mini.local>
2020-10-16 23:47:54 +0300

    Heroku V08

--- orders.py
+++ orders.py
@@ -3,7 +3,8 @@ import cal
 
 # Päivän varausten listaus
 def get_work_list(date_id, time_frame):
-    sql = "SELECT O.task_type, O.description, O.price, C.address, C.postcode, C.city, C.name, C.phone, C.email, C.instructions FROM orders O, customers C WHERE O.customer_id=C.id AND O.date_id=:date_id AND O.time_frame=:time_frame"
+    sql = "SELECT O.task_type, O.description, O.price, C.address, C.postcode, C.city, C.name, C.phone, C.email, C.instructions FROM orders O, customers C " \
+        "WHERE O.customer_id=C.id AND O.date_id=:date_id AND O.time_frame=:time_frame"
     result = db.session.execute(sql, {"date_id":date_id, "time_frame":time_frame})
     olist=result.fetchall()
     return olist
@@ -11,27 +12,33 @@ def get_work_list(date_id, time_frame):
 # Viikon varausten haku
 def get_orders(week_nr, max):
     varaukset = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    sql = "SELECT C.day_nr, O.time_frame, SUM(time_required) FROM calendar C, orders O WHERE C.week_nr=:week_nr AND C.day_nr < 6 AND O.date_id=C.id GROUP BY C.day_nr, O.time_frame"
+    sql = "SELECT C.id, C.day_nr, O.time_frame, SUM(time_required) FROM calendar C, orders O " \
+        "WHERE C.week_nr=:week_nr AND C.day_nr < 6 AND O.date_id=C.id GROUP BY C.id, C.day_nr, O.time_frame"
     result = db.session.execute(sql, {"week_nr":week_nr})
     olist=result.fetchall()
+    today_id = cal.get_today_id()
     for ol in olist:
-        iday = int(ol[0])
-        itfr = int(ol[1])
-        isum = int(ol[2])
-        if (isum > max):
-            varaukset[(iday-1)*3 + itfr - 1] = 1   
-    pweek = cal.get_week()
-    if (week_nr == pweek):
-        wday = cal.get_today().isoweekday()
-        for day in range(wday):
-            varaukset[3*day] = 1
-            varaukset[3*day + 1] = 1
-            varaukset[3*day + 2] = 1
+        iday = int(ol[1])
+        itfr = int(ol[2])
+        isum = int(ol[3])
+        if isum > max:
+            varaukset[(iday-1)*3 + itfr - 1] = 1
+        if ol[0] <= today_id:
+            varaukset[(iday-1)*3  + itfr -1] = 1 
+# Menneet päivät  
+#   pweek = cal.get_week()
+#    if (week_nr == pweek):
+#        wday = cal.get_today().isoweekday()
+#        for day in range(wday):
+#            varaukset[3*day] = 1
+#            varaukset[3*day + 1] = 1
+#            varaukset[3*day + 2] = 1
     return varaukset
 
 # Uuden tilauksen talletus tietokantaan
 def insert(cust_id, date_id, time_fr, ttype, desc, time_req, price, discount):
-    sql = "INSERT INTO orders (customer_id, date_id, time_frame, task_type, description, time_required, price, discount) VALUES (:cust_id, :date_id, :time_fr, :ttype, :desc, :time_req, :price, :discount)"
+    sql = "INSERT INTO orders (customer_id, date_id, time_frame, task_type, description, time_required, price, discount) " \
+        "VALUES (:cust_id, :date_id, :time_fr, :ttype, :desc, :time_req, :price, :discount)"
     db.session.execute(sql, {"cust_id":cust_id, "date_id":date_id, "time_fr":time_fr, "ttype":ttype, "desc":desc, "time_req":time_req, "price":price, "discount":discount})
     db.session.commit()
     return True
--- prices.py
+++ prices.py
@@ -3,20 +3,47 @@ import cal
 
 def prices(day_id, noutolaji):
     if (noutolaji == "1"):
-        hinnat = [59,59,59,59,59]
+        hinnat = [59,59,59,59,59,59,59,59,59,59,59,59,59,59,59]
     elif (noutolaji == "2"):
-        hinnat = [99,99,99,99,99]
+        hinnat = [99,99,99,99,99,99,99,99,99,99,99,99,99,99,99]
     elif(noutolaji == "3"):
-        hinnat = [149,149,149,149,149]
+        hinnat = [149,149,149,149,149,149,149,149,149,149,149,149,149,149,149]
+
     today_id = cal.get_today_id()
     for day in range(5):
         if (day_id - today_id < 8):
-            hinnat[day] += 20
+            hinnat[day*3] += 20
+            hinnat[day*3+1] += 20
+            hinnat[day*3+2] += 20
         elif (day_id - today_id < 15):
-            hinnat[day] += 10
+            hinnat[day*3] += 10
+            hinnat[day*3+1] += 10
+            hinnat[day*3+2] += 10
         day_id += 1
     return hinnat
 
+def wprices(week_nr, noutolaji):
+    if (noutolaji == "1"):
+        hinnat = [59,59,59,59,59,59,59,59,59,59,59,59,59,59,59]
+    elif (noutolaji == "2"):
+        hinnat = [99,99,99,99,99,99,99,99,99,99,99,99,99,99,99]
+    elif(noutolaji == "3"):
+        hinnat = [149,149,149,149,149,149,149,149,149,149,149,149,149,149,149]
+
+    sql = "SELECT C.day_nr, P.p1, P.p2, P.p3 FROM calendar C, prices P WHERE C.week_nr=:week_nr AND C.day_nr < 6 AND P.date_id=C.id"
+    result = db.session.execute(sql, {"week_nr":week_nr})
+    olist=result.fetchall()
+
+    for ol in olist:
+        iday = int(ol[0])
+        p1 = int(ol[1])
+        p2 = int(ol[2])
+        p3 = int(ol[3])
+        hinnat[(iday-1)*3] += p1 
+        hinnat[(iday-1)*3 + 1] += p2 
+        hinnat[(iday-1)*3 + 2] += p3 
+
+    return hinnat
 
 def get_price(day_id, noutolaji):
     if (noutolaji == "1"):
