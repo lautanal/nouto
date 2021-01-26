5d21357c118cb69f1b458f5db608a86dec3bb559
Lasse Lautanala <lasselautanala@gmail.com>
2021-01-26 22:01:32 +0200

    Heroku 210126

--- routes.py
+++ routes.py
@@ -66,7 +66,7 @@ def ajanvaraus():
         p_extra = 20
 
     if not p_accepted:
-        return render_template("index.html", noutolaji=noutolaji, kuvaus=kuvaus, postinumero=postinumero, error="Postinumero hakualueen ulkopuolella")
+        return render_template("index.html", noutolaji=noutolaji, kuvaus=kuvaus, postinumero=postinumero, error="Postinumero hakualueen ulkopuolella", error2="Voit kokeilla tilata noudon omalle alueellesi infopuhelimestamme 0600391500")
 
     viikko_nr = cal.get_week()
     paivat = cal.get_days(viikko_nr)
